61de605d65f7a4ec1094b9689364bd9b276eee1b
Zer0Legion <lengkhai@gmail.com>
2024-02-25 15:22:56 +0800

    feat: implement i/o parsing.

--- balancer/Balancer.java
+++ balancer/Balancer.java
@@ -1,8 +1,28 @@
+package balancer;
+
+import balancer.io.Parser;
+
 import java.util.Scanner;
 
-public class Main {
+/**
+ * The main class for the Balancer app.
+ * The input/output will be in the format provided by the Word document.
+ */
+public class Balancer {
+    /**
+     * The main method to run the program.
+     * @param args Any additional arguments will be ignored.
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        Parser parser = new Parser();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.isBlank()) {
+                break;
+            } else {
+                parser.parse(input);
+            }
+        }
     }
 }
--- balancer/io/Parser.java
+++ balancer/io/Parser.java
@@ -1,2 +1,61 @@
-package balancer.io;public class Parser {
+package balancer.io;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+/**
+ * Class to handle text parsing related to the Balancer app.
+ *
+ */
+public class Parser {
+    private static final String INCORRECT_FORMAT = "Please input an entry like this!\n{name},{amount paid}";
+
+    /**
+     * HashMap to store the payment mappings. It is chosen to handle multiple inputs from the same name.
+     */
+    private HashMap<String, Float> payments;
+
+    private float total;
+    private int nPax;
+
+    public Parser() {
+        this.payments = new HashMap<>();
+        this.total = 0;
+        this.nPax = 0;
+    }
+
+    /**
+     * Handles the logic for a payment input. Allows for multiple inputs with the same payer.
+     * @param name The person who paid for the item.
+     * @param paid The amount paid.
+     */
+    private void handlePayment(String name, float paid) {
+        if (payments.containsKey(name)) {
+            payments.put(name, payments.get(name) + paid);
+        } else {
+            payments.put(name, paid);
+            nPax++;
+        }
+        total += paid;
+    }
+
+    /**
+     * Parses a raw input string from the user.
+     * @param str The string to be parsed.
+     */
+    public void parse(String str) {
+        String[] params = str.split(",");
+        if (params.length != 2) {
+            System.out.println(INCORRECT_FORMAT);
+        } else {
+            String name = params[0].strip().toLowerCase();
+            try {
+                float paid = Float.parseFloat(params[1]);
+                handlePayment(name, paid);
+            } catch (NumberFormatException e) {
+                System.out.println(INCORRECT_FORMAT);
+            }
+        }
+    }
 }
